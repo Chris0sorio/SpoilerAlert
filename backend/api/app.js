29f0cbd991a6772d024e775952f2bd937d73415c
Chris Osorio <chrisosorio1011@gmail.com>
2024-10-05 13:03:09 -0400

    Updated app.js
    added requirements and initialization

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,1 +1,12 @@
 require('dotenv').config();
+
+//Required modules
+const axios = require('axios');
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+//Initialize Express
+const app = express();
+const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
+const MONGO_URL = process.env.MONGO_URL;
